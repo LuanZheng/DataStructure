5bb8beac2c5c18b0f52e6322b3e885850ed1b19d
栾征 <luanzheng@huazhu.com>
2017-04-19 16:56:51 +0800

    commit for directInsertSort

--- Sort.h
+++ Sort.h
@@ -3,25 +3,27 @@
 
 #include "Constant.h"
 
+template <class T>
 class Sort
 {
 public:
 	Sort();
-	void directInsertSort();
-	void simpleSelectSort();
-	void bubbleSort();
-	void combSort();
-	void shellSort();
-	void quickSort(int low, int high);
-	void mergeSort(int low, int high);
-	void print();
+	~Sort();
+	void directInsertSort(T* const sortArray, const unsigned int size);
+	//void simpleSelectSort();
+	//void bubbleSort();
+	//void combSort();
+	//void shellSort();
+	//void quickSort(int low, int high);
+	//void mergeSort(int low, int high);
+	void print(T* const sortArray, const unsigned int size);
 private:
-	void merge(int low, int high);
-	void initArray();
-	void directInsertSortForShellSort(int step, int startIndex);
-	int a[SORT_ARRAY_SIZE];
-	int loopTimes;
-	int moveTimes;
+	//void merge(int low, int high);
+	//void initArray();
+	//void directInsertSortForShellSort(int step, int startIndex);
+	/*T a[SORT_ARRAY_SIZE];*/
+	unsigned int loopTimes;
+	unsigned int moveTimes;
 };
 
 #endif
